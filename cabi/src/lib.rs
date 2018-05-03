fe23219b7400f865ae212fd09878758969ad7a18
messense <messense@icloud.com>
2018-05-03 15:26:52 +0800

    Add Python trainer API

--- cabi/src/lib.rs
+++ cabi/src/lib.rs
@@ -4,7 +4,7 @@ extern crate crfsuite;
 use std::{fmt, mem, ptr, slice, str};
 use std::boxed::Box;
 use std::ffi::CStr;
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_int};
 
 #[macro_use]
 mod utils;
@@ -34,6 +34,12 @@ impl fmt::Display for ErrorKind {
             ErrorKind::Panic(ref err) => err.fmt(f),
             ErrorKind::CrfError(ref err) => err.fmt(f),
         }
+    }
+}
+
+impl From<crfsuite::CrfError> for ErrorKind {
+    fn from(err: crfsuite::CrfError) -> ErrorKind {
+        ErrorKind::CrfError(err)
     }
 }
 
@@ -177,12 +183,12 @@ pub unsafe extern "C" fn pycrfsuite_err_clear() {
 
 pub struct Model;
 pub struct Tagger;
+pub struct Trainer;
 
 ffi_fn! {
     unsafe fn pycrfsuite_model_open(s: *const c_char) -> Result<*mut Model> {
         let path_cstr = CStr::from_ptr(s);
-        let model = crfsuite::Model::from_file(path_cstr.to_str().unwrap())
-            .map_err(ErrorKind::CrfError)?;
+        let model = crfsuite::Model::from_file(path_cstr.to_str().unwrap())?;
         Ok(Box::into_raw(Box::new(model)) as *mut Model)
     }
 }
@@ -199,7 +205,7 @@ ffi_fn! {
 ffi_fn! {
     unsafe fn pycrfsuite_tagger_create(m: *mut Model) -> Result<*mut Tagger> {
         let model = m as *mut crfsuite::Model;
-        let tagger =(*model).tagger().map_err(ErrorKind::CrfError)?;
+        let tagger =(*model).tagger()?;
         Ok(Box::into_raw(Box::new(tagger)) as *mut Tagger)
     }
 }
@@ -255,7 +261,7 @@ ffi_fn! {
             x.push(attrs);
         }
         let tagger = t as *mut crfsuite::Tagger;
-        let labels = (*tagger).tag(&x).map_err(ErrorKind::CrfError)?;
+        let labels = (*tagger).tag(&x)?;
         let mut tags: Vec<FfiStr> = labels.into_iter()
             .map(|l| FfiStr::from_string(l))
             .collect();
@@ -265,5 +271,128 @@ ffi_fn! {
         mem::forget(tags);
         let c_tags = Tags { data: buffer, len: tag_count};
         Ok(Box::into_raw(Box::new(c_tags)))
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_create() -> Result<*mut Trainer> {
+        let trainer = crfsuite::Trainer::new();
+        Ok(Box::into_raw(Box::new(trainer)) as *mut Trainer)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_destroy(trainer: *mut Trainer) {
+        if !trainer.is_null() {
+            let trainer = trainer as *mut crfsuite::Trainer;
+            Box::from_raw(trainer);
+        }
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_select(trainer: *mut Trainer, algo: *const c_char) -> Result<()> {
+        let algorithm = CStr::from_ptr(algo)
+            .to_str()
+            .unwrap()
+            .parse::<crfsuite::Algorithm>()?;
+        let trainer = trainer as *mut crfsuite::Trainer;
+        Ok((*trainer).select(algorithm, crfsuite::GraphicalModel::CRF1D)?)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_clear(trainer: *mut Trainer) -> Result<()> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        Ok((*trainer).clear()?)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_train(trainer: *mut Trainer, model: *const c_char, holdout: c_int) -> Result<()> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let model_str = CStr::from_ptr(model).to_str().unwrap();
+        Ok((*trainer).train(model_str, holdout as i32)?)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_append(trainer: *mut Trainer, xseq: *const AttributeList, xseq_len: usize, yseq: *const *const c_char, yseq_len: usize, group: c_int) -> Result<()> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let items = slice::from_raw_parts(xseq, xseq_len);
+        let mut x = Vec::with_capacity(items.len());
+        for item in items {
+            let attr_slice = slice::from_raw_parts(item.data, item.len);
+            let attrs: Vec<crfsuite::Attribute> = attr_slice.iter()
+                .map(|attr| crfsuite::Attribute::new(CStr::from_ptr(attr.name).to_string_lossy().to_owned(), attr.value))
+                .collect();
+            x.push(attrs);
+        }
+        let items = slice::from_raw_parts(yseq, yseq_len);
+        let mut y = Vec::with_capacity(items.len());
+        for item in items {
+            let tag = CStr::from_ptr(*item).to_str().unwrap();
+            y.push(tag);
+        }
+        Ok((*trainer).append(&x, &y, group)?)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_set(trainer: *mut Trainer, name: *const c_char, value: *const c_char) -> Result<()> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let name_str = CStr::from_ptr(name).to_str().unwrap();
+        let value_str = CStr::from_ptr(value).to_str().unwrap();
+        Ok((*trainer).set(name_str, value_str)?)
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_get(trainer: *mut Trainer, name: *const c_char) -> Result<FfiStr> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let name_str = CStr::from_ptr(name).to_str().unwrap();
+        let value = (*trainer).get(name_str)?;
+        Ok(FfiStr::from_string(value))
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_help(trainer: *mut Trainer, name: *const c_char) -> Result<FfiStr> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let name_str = CStr::from_ptr(name).to_str().unwrap();
+        let value = (*trainer).help(name_str)?;
+        Ok(FfiStr::from_string(value))
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct Params {
+    pub data: *mut FfiStr,
+    pub len: usize,
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_trainer_params(trainer: *mut Trainer) -> Result<*mut Params> {
+        let trainer = trainer as *mut crfsuite::Trainer;
+        let params = (*trainer).params();
+        let mut params: Vec<FfiStr> = params.into_iter()
+            .map(|l| FfiStr::from_string(l))
+            .collect();
+        params.shrink_to_fit();
+        let param_count = params.len();
+        let buffer = params.as_mut_ptr();
+        mem::forget(params);
+        let c_params = Params { data: buffer, len: param_count};
+        Ok(Box::into_raw(Box::new(c_params)))
+    }
+}
+
+ffi_fn! {
+    unsafe fn pycrfsuite_params_destroy(params: *mut Params) {
+        if !params.is_null() {
+            Vec::from_raw_parts((*params).data, (*params).len, (*params).len);
+            Box::from_raw(params);
+        }
     }
 }
