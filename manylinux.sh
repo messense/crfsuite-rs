69056df756e22022c8b8a0d8406da3353d7b7aee
messense <messense@icloud.com>
2020-11-03 11:31:08 +0800

    Stop using linux32

--- manylinux.sh
+++ manylinux.sh
@@ -9,8 +9,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=~/.cargo/bin:$PATH
 
 # Build wheels
-which linux32 && LINUX32=linux32
-$LINUX32 /opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
+/opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
 
 # Audit wheels
 for wheel in dist/*-linux_*.whl; do
