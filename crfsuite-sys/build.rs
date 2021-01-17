6f0590185e430b65ec0b4332651ab5714ab3ad01
messense <messense@icloud.com>
2021-01-17 22:59:03 +0800

    Set CMAKE_OSX_ARCHITECTURES to x86_64 when build for x86_64-apple-darwin
    Fixes build error on Apple Silicon cause cargo can't link universal
    archive

--- crfsuite-sys/build.rs
+++ crfsuite-sys/build.rs
@@ -12,7 +12,15 @@ fn fail_on_empty_directory(name: &str) {
 }
 
 fn build_crfsuite() {
-    let dst = cmake::Config::new("").register_dep("lbfgs").build();
+    let mut cfg = cmake::Config::new("");
+    cfg.register_dep("lbfgs");
+    if cfg!(target_os = "macos") {
+        let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+        if target_arch == "x86_64" {
+            cfg.define("CMAKE_OSX_ARCHITECTURES", "x86_64");
+        }
+    }
+    let dst = cfg.build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=cqdb");
     println!("cargo:rustc-link-lib=static=lbfgs");
